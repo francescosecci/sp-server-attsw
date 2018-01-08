3ba05ccf6b0808be06eb924cef992ca1e867c2f5
Alex Foglia <alex.f1793@gmail.com>
2018-01-08 14:36:45 +0100

    organized some imports

--- server/src/test/java/com/pufose/server/ARestControllerTest.java
+++ server/src/test/java/com/pufose/server/ARestControllerTest.java
@@ -8,8 +8,6 @@ import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.BDDMockito.*;
 import java.util.Arrays;
 import java.util.List;
-
-import org.hamcrest.CoreMatchers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
