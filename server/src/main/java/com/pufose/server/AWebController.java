fff2860cc13b915ba8ee5426bf2aeda0c26a4887
Alex Foglia <alex.f1793@gmail.com>
2018-01-10 14:25:37 +0100

    implemented first method of webcontroller

--- server/src/main/java/com/pufose/server/AWebController.java
+++ server/src/main/java/com/pufose/server/AWebController.java
@@ -1,6 +1,18 @@
 package com.pufose.server;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class AWebController {
-	
+	@Autowired
+	private IGridService service;
+	@RequestMapping("/")
+    public String welcome(Map<String,Object> model) {
+        return "database";
+    }
 
 }
--- server/src/test/java/com/pufose/server/AWebControllerTest.java
+++ server/src/test/java/com/pufose/server/AWebControllerTest.java
@@ -1,25 +1,66 @@
 package com.pufose.server;
 
-import static org.junit.Assert.*;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.security.Principal;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.mock.web.MockHttpSession;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+
 
 @RunWith(SpringRunner.class)
 @WebMvcTest(controllers=AWebController.class)
-public class AWebControllerTest {
+@Import(WebSecurityConfig.class)
+public class AWebControllerTest  {
+	
 	@Autowired
-	private MockMvc mvc;
+    private WebApplicationContext wac;
+
+    @Autowired
+    private FilterChainProxy springSecurityFilter;
+
+    protected MockMvc mockMvc;
+    protected MockHttpSession session;
+    
+    @Before
+    public void setup() throws Exception{
+        this.session = new MockHttpSession();
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac)
+                .addFilters(springSecurityFilter)
+                .build();
+    }
+
 	@MockBean
 	private IGridService gridService;
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testLogin401() throws Exception {
+		
+		mockMvc.perform(get("/")).andExpect(status().is(401));
+	}
+	@Test
+	public void testLoginOk() throws Exception{
+		mockMvc.perform(get("/").with(httpBasic("user","password"))).andExpect(
+				status().isOk());
+				
+		
 	}
 
 }
