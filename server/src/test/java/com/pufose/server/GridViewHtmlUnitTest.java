e5b50039755c0a93d9cedafc12b7fe19eda8ee12
tom <tommasopuccetti@gmail.com>
2018-01-11 13:28:35 +0100

    Added HtmlUnit dependency

---
+++ server/src/test/java/com/pufose/server/GridViewHtmlUnitTest.java
@@ -0,0 +1,14 @@
+package com.pufose.server;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GridViewHtmlUnitTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
