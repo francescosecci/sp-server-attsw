ce20fced06c94dcf1e3cf5ee6bd8544862c84919
Alex Foglia <alex.f1793@gmail.com>
2018-01-19 15:24:02 +0100

    update pom, properties and create sql config class

---
+++ server/src/main/java/com/pufose/server/DatabaseConfig.java
@@ -0,0 +1,60 @@
+package com.pufose.server;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+public class DatabaseConfig {
+	@Value("${spring.datasource.driver-class-name}")
+	private String DB_DRIVER;
+	
+	@Value("${spring.datasource.url}")
+	private String DB_URL;
+	
+	@Value("${spring.datasource.username}")
+	private String username;
+	@Value("${spring.datasource.password}")
+	private String password;
+	
+	@Value("${spring.jpa.properties.hibernate.dialect}")
+	private String dialect;
+	
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	    dataSource.setDriverClassName(DB_DRIVER);
+	    dataSource.setUrl(DB_URL);
+	    dataSource.setUsername(username);
+	    dataSource.setPassword(password);
+	    return dataSource;
+	}
+	@Bean
+	  public LocalSessionFactoryBean sessionFactory() {
+	    LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+	    sessionFactoryBean.setDataSource(dataSource());
+	    Properties hibernateProperties = new Properties();
+	    hibernateProperties.put("hibernate.dialect", dialect);
+	    sessionFactoryBean.setHibernateProperties(hibernateProperties);
+	    return sessionFactoryBean;
+	  }
+
+	  @Bean
+	  public HibernateTransactionManager transactionManager() {
+	    HibernateTransactionManager transactionManager = 
+	        new HibernateTransactionManager();
+	    transactionManager.setSessionFactory(sessionFactory().getObject());
+	    return transactionManager;
+	  }
+
+
+}
