9ab1f1ef8b0699218e5a5aa3c8ed12772ab04547
francescosecci <i_secci@hotmail.it>
2018-02-05 15:12:28 +0100

    fix app properties

--- server/src/main/java/com/pufose/server/ServerApplication.java
+++ server/src/main/java/com/pufose/server/ServerApplication.java
@@ -4,9 +4,11 @@ package com.pufose.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.test.context.ActiveProfiles;
+
+
 @SpringBootApplication
-
-
+@ActiveProfiles("mysql")
 public class ServerApplication {
 
 	
