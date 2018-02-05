f2bd59f9e9304b6651379bd5a63ba0ff2afd3500
Alex Foglia <alex.f1793@gmail.com>
2018-02-05 13:51:27 +0100

    fixed application properties

--- server/src/main/java/com/pufose/server/ServerApplication.java
+++ server/src/main/java/com/pufose/server/ServerApplication.java
@@ -1,12 +1,12 @@
 package com.pufose.server;
 
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.ActiveProfiles;
-
 @SpringBootApplication
-@ActiveProfiles("mysql")
+
+
 public class ServerApplication {
 
 	
