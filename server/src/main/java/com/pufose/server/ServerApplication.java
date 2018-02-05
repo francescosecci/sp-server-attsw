7027d5ca6460c7df7892b5c56664eb2549837ef5
Alex Foglia <alex.f1793@gmail.com>
2018-02-05 19:21:08 +0100

    programmatically load profile mongo in main

--- server/src/main/java/com/pufose/server/ServerApplication.java
+++ server/src/main/java/com/pufose/server/ServerApplication.java
@@ -2,20 +2,30 @@ package com.pufose.server;
 
 
 
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.core.env.AbstractEnvironment;
 
 
 @SpringBootApplication
-@ActiveProfiles("mongo")
-public class ServerApplication {
+public class ServerApplication extends SpringBootServletInitializer{
 
 	
-	public static void main(String[] args) {
-		SpringApplication.run(ServerApplication.class, args);
-		
-	}
+
+    public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "mongo");
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        System.setProperty(AbstractEnvironment.ACTIVE_PROFILES_PROPERTY_NAME, "dev"); 
+        super.onStartup(servletContext);
+    }
 	
 	
 }
